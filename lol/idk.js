db8b9d45a68f9a265373d5ebc3b85b5090411f6a
Michaela Ptáčková <98548929+michaellkaa@users.noreply.github.com>
2022-06-27 18:34:49 +0200

    Add files via upload

--- lol/idk.js
+++ lol/idk.js
@@ -2,16 +2,23 @@ var root = document.querySelector(':root');
 
 function setColorMode() {
     var elementBody = document.body;
-    
-    if (localStorage.getItem("colorMode") == "dark") {
+
+    var logo = document.getElementById("logo");
+
+    logo  = function() {
+    if (localStorage.getItem("colorMode") === "dark-mode") {
         elementBody.classList.add("dark-mode");
     } else {
         elementBody.classList.remove("dark-mode");
-    }
+        }
+    };
 
 
-
+    console.log(localStorage.getItem("colorMode"));
 };
+
+
+
 
 
 function changeColorMode () {
@@ -29,28 +36,14 @@ function changeColorMode () {
 
 
 
-
-
-
-let logo = document.getElementById('logo');
-
-let darkImageURL = "logo-black_b.png";
-let lightImageURL = "logo-white_t.png";
-
-document.getElementById("dark-mode").addEventListener('click', function(event) {
-    var element = document.body;
-
-    if (this.innerHTML === "Dark") {
-      element.dataset.theme = "dark";
-      this.innerHTML = "Light";
-    } else {
-      element.dataset.theme = "light";
-      this.innerHTML = "Dark";
-    }
-});
-
 console.log(localStorage.getItem("colorMode"));
 
-
 setColorMode();
+
+
+
+
+function newFunction() {
+    console.log(localStorage.getItem("colorMode"));
+}
 
